845ea7548d482c65e9a2871232c000c8dd48ab80
Michael Melis <michael.melis169@gmail.com>
2022-02-11 12:31:24 +0100

    feat:  google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,21 @@ module.exports = {
     title: "Michael-Melis-Portfolio-2022",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-G5RWKJL5CM", // Google Analytics / GA
+        ],
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-fontawesome-css`,
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
